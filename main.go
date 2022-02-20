334110072fc3a18751c25b9b0e862bb44023b31f
hizuheka <44028436+hizuheka@users.noreply.github.com>
2022-02-20 16:12:34 +0900

    とりあえず完成。

--- main.go
+++ main.go
@@ -1,121 +1,131 @@
 package main
 
 import (
-  "os"
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
 
-  "github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2"
 )
 
 type File struct {
-	path string
-	size string
+	path string // ファイルパス
+	size string // ファイルサイズ
 }
 
+type Unmatch struct {
+	path   string // ファイルのパス
+	reason string // 不一致理由
+}
+
+const (
+	UnmatchReasonNonExist    = "ファイルなし"
+	UnmatchReasonSizeUnmatch = "ファイルサイズ不一致"
+)
+
 func main() {
-	var dbname string
-	var sqlF string
-	var sql string
-	var input string
-	var output string
+	var baseDir, source, dest, output string
+	var numConcret int
 
 	app := &cli.App{
-		Name: "MuscleRdbunlsql",
+		Name:    "pjkakuninja",
 		Version: Version,
-		Usage: "力技でrdbunlsql",
-		Flags: []cli.Flag {
-			&cli.StringFlag{
-				Name: "dbname",
-				Aliases: []string{"d"},
-				Usage: "データを取り出すデータベースの名前 `DBNAME` を指定します。",
-				Destination: &dbname,
-				Required: true,
+		Usage:   "P-WEB確認ジャ！",
+		Flags: []cli.Flag{
+			&cli.IntFlag{
+				Name:        "num-concrent",
+				Aliases:     []string{"c"},
+				Usage:       "並列処理数 `NUM_CONCRENT` を指定します。未指定の場合、CPU数の半分が設定されます。",
+				Destination: &numConcret,
 			},
 			&cli.StringFlag{
-				Name: "sqlF",
-				Aliases: []string{"v"},
-				Usage: "SQLを記述したファイルのパス名(絶対パス) `SQL_FILE_PATH` を指定します。",
-				Destination: &sqlF,
+				Name:        "baseDir",
+				Aliases:     []string{"b"},
+				Usage:       "チェック先フォルダのパス `BASE_DIR` を指定します。",
+				Destination: &baseDir,
+				Required:    true,
 			},
 			&cli.StringFlag{
-				Name: "sql",
-				Aliases: []string{"s"},
-				Usage: "`SQL` を指定します。",
-				Destination: &sql,
+				Name:        "source",
+				Aliases:     []string{"s"},
+				Usage:       "比較元ファルのパス `SOURCE_FILE_PATH` を指定します。",
+				Destination: &source,
+				Required:    true,
 			},
 			&cli.StringFlag{
-				Name: "input",
-				Aliases: []string{"i"},
-				Usage: "データを入力するファイルのパス `INPUT_FILE_PATH` を指定します。",
-				Destination: &input,
-				Required: true,
+				Name:        "dest",
+				Aliases:     []string{"d"},
+				Usage:       "比較先ファルのパス `DEST_FILE_PATH` を指定します。",
+				Destination: &dest,
+				Required:    true,
 			},
 			&cli.StringFlag{
-				Name: "output",
-				Aliases: []string{"o"},
-				Usage: "データを出力するファイルのパス `OUTPUT_FILE_PATH` を指定します。",
+				Name:        "output",
+				Aliases:     []string{"o"},
+				Usage:       "データを出力するファイルのパス `OUTPUT_FILE_PATH` を指定します。",
 				Destination: &output,
-				Required: true,
+				Required:    true,
 			},
 		},
-		Before: func(c *cli.Context) error {
-			if !c.IsSet("sqlF") && !c.IsSet("sql") {
-				ec := cli.Exit("sqlF か sql のどちらかを指定する必要があります。", 1)
-				return ec
-			}
-		}
 		Action: func(c *cli.Context) error {
-			name := "Nefertiti"
-			if c.NArg() > 0 {
-				name = c.Args().Get(0)
+			// チェック結果を出力するファイル。既にファイルが存在する場合は削除
+			outFp, err := os.OpenFile(output, os.O_CREATE|os.O_TRUNC, 0644)
+			if err != nil {
+				return cli.Exit(err, 1)
 			}
-			if c.String("lang") == "spanish" {
-				fmt.Println("Hola", name)
-			} else {
-				fmt.Println("Hello", name)
+			defer outFp.Close()
+
+			// チェック結果を書き出す専用のゴルーチン
+			resultsCh := make(chan Unmatch, 50) // アンマッチファイルを書き出すためのチャネル
+			done := make(chan struct{})         // ファイル出力終了を伝えるためのチャネル
+			go writeUnMatchFile(resultsCh, outFp, done)
+
+			// チェック先のファイル
+			destFp, err := os.Open(dest)
+			if err != nil {
+				return cli.Exit(err, 1)
 			}
-			return nil
-			// ec := cli.Exit("ohwell", 86)
-			// fmt.Fprintf(c.App.Writer, "%d", ec.ExitCode())
-			// fmt.Printf("made it!\n")
-			// return ec
+			defer destFp.Close()
 
-			// チャネル src から受信した rdbunlsql の実行に必要な情報を基に、rdbunlsql を実行する。
-			// rdbunlsql の実行は Workers パターンを使用するが、チャネル src の順番で w に出力する
-			// ために、バッファありチャネル dataChs を使用する。
+			// チェック先ファイルからチェック用のハッシュマップを生成する
+			destMap, err := createDestMap(destFp)
+			if err != nil {
+				return cli.Exit(err, 1)
+			}
 
-			// rdbunlsql の実行結果を直列化して書き出すためのチャネル
-			dataChs := make(chan chan []byte, 50)
-			// 実行対象の rdbunlsql 情報をためるチャネル
-			unlsqls := make(chan Unlsql, 50)
+			// チェック元
+			srcFp, err := os.Open(source)
+			if err != nil {
+				return cli.Exit(err, 1)
+			}
+			defer srcFp.Close()
+			sourceCh := gen(srcFp, baseDir)
 
-			// rdbunlsql実行結果を直列化して書き出す専用のゴルーチン
-			done := make(chan struct{})
-			go writeInOrder(dataChs, w, done)
+			// NUM_CONCURRENT が未指定の場合は、CPU数の半分とする。
+			if numConcret == 0 {
+				numConcret = runtime.NumCPU() / 2
+			}
 
-			// CPU数に応じたワーカーを生成
-			// ワーカーの数をCPU数の半分にしているのは、CPU数の半分の方が処理速度が早いと書いてある
-			// サイトがあったから
+			// ワーカーを生成
 			var wg sync.WaitGroup
-			for i := 0; i < runtime.NumCPU()/2; i++ {
+			for i := 0; i < numConcret; i++ {
 				wg.Add(1)
-				go worker(i+1, unlsqls, errCh, &wg)
+				go worker(sourceCh, destMap, resultsCh, &wg)
 			}
-
-			// src から受信したrdbunlsql情報を、ワーカーに渡すためのチャネル unlsqls と dataChs に
-			// 格納していく。
-			go func() {
-				for u := range src {
-					dataChs <- u.dataCh
-					unlsqls <- u
-				}
-				close(dataChs)
-				close(unlsqls)
-			}()
-
 			wg.Wait()
 
+			// ワーカーがすべて完了すると、resultsCh への送信が完了するのでクローズする
+			close(resultsCh)
+
+			// writeUnMatchFile が完了するまで待機
 			<-done
 
+			return nil
 		},
 	}
 
@@ -125,131 +135,164 @@ func main() {
 	}
 }
 
-func rdbunlsql(db string, sql string, path string) error {
-	out, err  := exec.Command("rdbunlsql", "-d", db, "-s", sql, "-t", path).CombinedOutput();
-	if err != nil {
-		return fmt.Errorf("failed to rdbunlsql: %w, msg: %s", err, out)
-	}
-
-	return nil
-}
-
-func createSearchMap(r io.Reader) (map[string]string, error) {
+// r で指定されたファイルから、チェック用のマップを生成する。
+// r の1行は次の構成。
+// "ファイル名","ファイルのフルパス","ファイルの拡張子",ファイルサイズ,フォルダフラグ(フォルダの場合true),更新日,更新時刻
+func createDestMap(r io.Reader) (map[string]string, error) {
 	m := make(map[string]string)
+	var read, skip, add uint
 
- 	s := bufio.NewScanner(r)
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		read += 1
+		if s.Text() == "" {
+			skip += 1
+			continue
+		}
 
- 	for s.Scan() {
- 		ary := strings.Split(s.Text(), ",")
-
+		ary := strings.Split(s.Text(), ",")
 		if len(ary) != 7 {
 			return nil, fmt.Errorf("テンポラリストレージのファイルリストのフォーマット不正. len=%d", len(ary))
 		}
 
+		// サイズが0の場合はチェック対象外のためスキップする
+		if ary[3] == "0" {
+			skip += 1
+			continue
+		}
+
 		// パスの区切りは「/」とする
 		p := strings.Replace(ary[1], "\\", "/", -1)
+		// ファイルパスが”で括られているので削除する
+		p = strings.Replace(p, "\"", "", -1)
 		m[p] = ary[3]
- 	}
+
+		add += 1
+	}
 
 	if s.Err() != nil {
 		// non-EOF error.
 		return nil, s.Err()
 	}
 
+	// ファイル読み込み結果を出力
+	fmt.Println("◆チェック先ファイル(DEST_FILE_PATH)の読み込みを完了しました。")
+	fmt.Printf("　→読み込み件数 : %d\n", read)
+	fmt.Printf("　→検索用ファイル件数 : %d\n", add)
+	fmt.Printf("　→スキップ件数(サイズ=0) : %d\n", skip)
+
 	return m, nil
 }
 
-func gen(r io.Reader, letter string) (<-chan File) {
-	out := make(chan File, 50)
+// rで指定されたファイルを1行ずつ読み込み、File のチャネルを生成する。
+// rの1行の構成は次の通り。
+// "プロジェクト名","カテゴリ","サブカテゴリ",ファイルパス,ファイルサイズ
+func gen(r io.Reader, prifix string) <-chan File {
+	out := make(chan File, 50) // バッファ数50の根拠はなし
 
-	go func(br *bufio.Reader) {
+	// 「\」を「/」に置換する。置換した結果、末尾に「/」がない場合は付加する。
+	p := strings.Replace(prifix, "\\", "/", -1)
+	if !strings.HasSuffix(p, "/") {
+		p = p + "/"
+	}
+
+	go func(br *bufio.Reader, p string) {
 		defer close(out)
+
+		var read, skip, add uint
+
 		scanner := bufio.NewScanner(br)
 		for scanner.Scan() {
-			ary := strings.Split(s.Text(), ",")
-			aryP := ary[0:3]
-			size := ary[4]
-			path := strings.Join(aryP, "/")
+			read += 1
 
-			select {
-			case out <- File{path, size}:
-			default:
-				fmt.Println("入力ファイル用チャネルのバッファが不足しています.")
+			ary := strings.Split(scanner.Text(), ",")
+
+			// ファイルサイズが0は無視する。
+			if p == "0" {
+				skip += 1
+				continue
 			}
+
+			// ファイルパスの作成。”で括られているので削除する。
+			aryP := ary[0:4]
+			size := ary[4]
+			path := p + strings.Replace(strings.Join(aryP, "/"), "\"", "", -1)
+
+			out <- File{path, size}
+			add += 1
 		}
 
+		// 横着してエラーメッセージの出力のみで終わっている。。。
+		// 本当は、ワーカーやファイル出力のゴルーチンを終了させる必要がある。
 		if scanner.Err() != nil {
 			// non-EOF error.
-			return nil, scanner.Err()
+			fmt.Printf("チェック元ファイルの読み込みでエラーが発生しました.(%s)\n", scanner.Err())
+			return
 		}
-	}(bufio.NewReader(br))
+
+		// ファイル読み込み結果を出力する。
+		fmt.Println("◆チェック元ファイル(SOURCE_FILE_PATH)の読み込みを完了しました。")
+		fmt.Printf("　→読み込み件数 : %d\n", read)
+		fmt.Printf("　→検索用ファイル件数 : %d\n", add)
+		fmt.Printf("　→スキップ件数(サイズ=0) : %d\n", skip)
+	}(bufio.NewReader(r), p)
 
 	return out
 }
 
 // ワーカー
-func worker(i int, db string, src <-chan Unlsql, errCh chan error, wg *sync.WaitGroup) {
+func worker(fileCh <-chan File, destMap map[string]string, resultsCh chan<- Unmatch, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// タスクがなくなってタスクのチェネルがcloseされるまで無限ループ
-	for s := range src {
-		out, err := filepath.Abs(fmt.Sprintf("./data_%d", i)
-		if err != nil {
-			errCh <- err
-			return
+	for f := range fileCh {
+		isExist := true
+		msg := ""
+		if v, ok := destMap[f.path]; ok {
+			if v != f.size {
+				isExist = false
+				msg = UnmatchReasonSizeUnmatch
+			}
+		} else {
+			isExist = false
+			msg = UnmatchReasonNonExist
 		}
-		if err := rdbunlsql(db, s.sql, out) {
-			errCh <- err
-			return
+
+		if !isExist {
+			resultsCh <- Unmatch{f.path, msg}
+			// fmt.Printf("%s:%s\n", msg, f.path)
 		}
-		bytes, err := ioutil.ReadFile(out)
-		if err != nil {
-			errCh <- err
-			return
-		}
-		s.dataCh <- bytes
 	}
 }
 
-// 順番に従ってに書き出しをする(goroutineで実行される)
-func writeInOrder(dataChs <-chan chan []byte, w io.Writer, done chan<- struct{}) error {
-	bw := bufio.NewWriter(w)
-	defer bw.Flush()
-
+// w へアンマッチファイルのパスを出力する(goroutineで実行される)
+func writeUnMatchFile(resultsCh <-chan Unmatch, w io.Writer, done chan<- struct{}) error {
 	// 書き出し完了を表すチャネルをクローズする
 	defer close(done)
 
-	// 順番に取り出す
-	for dataCh := range dataChs {
-		// 選択された仕事が終わるまで待つ
-		data := <-dataCh
-		if _, err := bw.Write(data); err != nil {
+	var write, nonexists, sizeunmatch uint
+
+	bw := bufio.NewWriter(w)
+	defer bw.Flush()
+
+	// resultsCh が close するまで繰り返す
+	for p := range resultsCh {
+		if _, err := bw.WriteString(fmt.Sprintf("%s:%s\n", p.reason, p.path)); err != nil {
 			return err
 		}
-		close(dataCh)
+		write += 1
+		switch p.reason {
+		case UnmatchReasonNonExist:
+			nonexists += 1
+		case UnmatchReasonSizeUnmatch:
+			sizeunmatch += 1
+		}
 	}
+
+	// 結果を出力
+	fmt.Println("◆結果ファイル(OUTPUT_FILE_PATH)の書き込みを完了しました。")
+	fmt.Printf("　→出力件数 : %d\n", write)
+	fmt.Printf("　→ファイルなし : %d\n", nonexists)
+	fmt.Printf("　→サイズ不一致 : %d\n", sizeunmatch)
 
 	return nil
 }
-
-// func main() {
-// 	fmt.Printf("runtime.NumCPU()=%d\n", runtime.NumCPU())
-
-// 	m := bytes.NewBufferString(conmap)
-// 	conmap, err := genConvertMap(m)
-
-// 	//filename := os.Args[1]
-// 	//f, err := os.Open(filename)
-// 	//if err != nil {
-// 	//	log.Fatalf("cannot open file %q: %v", filename, err)
-// 	//}
-// 	//defer f.Close()
-// 	f := bytes.NewBufferString(target)
-// 	jefs, err := gen(f)
-// 	if err != nil {
-// 		log.Fatalf("cannot generate convert map: %v", err)
-// 	}
-
-// 	stdout := new(bytes.Buffer)
-// 	processSession(jefs, conmap, stdout)
-// 	fmt.Printf("%s -> %s", target, stdout.String())
-// }
